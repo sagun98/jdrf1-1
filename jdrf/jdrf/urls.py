93ff9077107ae26d7058692c7aa6e62b2997e222
ljmciver <lauren.j.mciver@gmail.com>
2017-10-13 00:22:03 +0000

    Add back in about page without login form since the login now has its own page

--- jdrf/jdrf/urls.py
+++ jdrf/jdrf/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^login/', TemplateView.as_view(template_name='login.html')),
+    url(r'^about/', TemplateView.as_view(template_name='about.html')),
 ]
