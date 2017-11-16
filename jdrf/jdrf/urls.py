3e3974948fafd0a9c5eaf0ef64617e853360f0a3
ljmciver <lauren.j.mciver@gmail.com>
2017-11-16 23:52:22 +0000

    Add base upload page visible only after login

--- jdrf/jdrf/urls.py
+++ jdrf/jdrf/urls.py
@@ -21,10 +21,13 @@ from django.views.generic import TemplateView
 
 from django.contrib.auth import views as auth_views
 
+from pages import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^login/', auth_views.LoginView.as_view(template_name='login.html'),name="login"),
     url(r'^logout/', auth_views.LogoutView.as_view(next_page="login"),name="logout"),
+    url(r'^upload/', views.upload,name="upload"),
     url(r'^about/', TemplateView.as_view(template_name='about.html'),name="about"),
 ]
--- jdrf/pages/views.py
+++ jdrf/pages/views.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.shortcuts import render
+
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login/')
+def upload(request):
+    return render(request,'upload.html')
