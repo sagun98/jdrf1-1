e5f8883f4b6a0532ff569cf99cb03f8c7ea4ef57
ljmciver <lauren.j.mciver@gmail.com>
2018-04-21 00:27:51 +0000

    Add default transfer settings

--- jdrf/jdrf/settings.py
+++ jdrf/jdrf/settings.py
@@ -18,6 +18,9 @@ UPLOAD_FOLDER = "/opt/upload_folder/"
 PROCESS_FOLDER = "/opt/process_folder/"
 ARCHIVE_FOLDER = "/opt/archive_folder/"
 TEMP_FOLDER = "/opt/temp/"
+SSH_KEY = "/opt/SSH/jdrf_transfer.pub"
+REMOTE_TRANSFER_SERVER = "jdrf2.rc.fas.harvard.edu"
+REMOTE_TRANSFER_FOLDER = "/data/"
 
 # set the default metadata file names
 METADATA_FILE_NAME = "metadata.tsv"
