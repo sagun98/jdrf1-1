91f48cde6ed6bda7bf2d8e8e04feab9c54a32227
ljmciver <lauren.j.mciver@gmail.com>
2018-04-21 00:30:34 +0000

    Update archive workflow to use default output folder to store database and log

--- jdrf/jdrf/archive_workflow.py
+++ jdrf/jdrf/archive_workflow.py
@@ -8,20 +8,19 @@ from anadama2 import Workflow
 from biobakery_workflows import utilities
 
 # create a workflow and get the arguments
-workflow = Workflow(remove_options=["input","output"])
+workflow = Workflow(remove_options=["input"])
 workflow.add_argument("input-upload", desc="the folder of raw uploaded data", required=True)
 workflow.add_argument("input-processed", desc="the folder of processed data", required=True)
 workflow.add_argument("key", desc="the key file to use for the transfer", required=True)
 workflow.add_argument("remote", desc="the remote host name for the transfer", required=True)
 workflow.add_argument("study", desc="the name of the study", required=True)
-workflow.add_argument("output-archive", desc="the folder to store the archived data", required=True)
 workflow.add_argument("output-transfer", desc="the folder to transfer the data", required=True)
 args = workflow.parse_args()
 
 # archive the raw input files
 date=datetime.datetime.now()
 study_folder=args.study+"_"+str(date.month)+"_"+str(date.day)+"_"+str(date.year)
-archive_folder = os.path.join(args.output_archive,study_folder)
+archive_folder = os.path.join(args.output,study_folder)
 
 upload_archive = archive_folder+"_uploaded"
 utilities.create_folders(upload_archive)
