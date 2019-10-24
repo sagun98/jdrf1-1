7eb766ea3ac4ce01daea9bf0b03507e83f891afa
ljmciver <lauren.j.mciver@gmail.com>
2019-10-24 12:48:31 -0600

    Add update counts plus transfer to archive and transfer script

--- jdrf/jdrf/archive_workflow.py
+++ jdrf/jdrf/archive_workflow.py
@@ -3,6 +3,11 @@ import os
 import sys
 import datetime
 
+# constants 
+ARCHIVE_FOLDER = "/opt/archive_folder/"
+COUNT_FILE = os.path.join(ARCHIVE_FOLDER,"data_deposition_counts.csv")
+PUBLIC_COUNT_FILE = os.path.join(ARCHIVE_FOLDER,"data_deposition_counts_public.csv")
+
 # create a workflow to check the md5sums for each file
 from anadama2 import Workflow
 from biobakery_workflows import utilities
@@ -16,6 +21,7 @@ workflow.add_argument("user", desc="the user id for the transfer", required=True
 workflow.add_argument("remote", desc="the remote host name for the transfer", required=True)
 workflow.add_argument("study", desc="the name of the study", required=True)
 workflow.add_argument("output-transfer", desc="the folder to transfer the data", required=True)
+workflow.add_argument("count-script", desc="the script to update the data stats", required=True)
 args = workflow.parse_args()
 
 # archive the raw input files
@@ -55,6 +61,20 @@ task5=workflow.add_task(
     "rsync --exclude '*.fastq*' --exclude '*.fq*' --exclude '*.fasta*' --exclude '*.fa*' --exclude '*.bam' --exclude '*.sam' --exclude '*.vcf*' --recursive -e 'ssh -i [args[0]] -l [args[1]]' [args[2]] [args[3]]:[args[4]]",
     depends=task4,
     args=[args.key, args.user, process_archive, args.remote, args.output_transfer])
+
+# update the stats
+task6=workflow.add_task(
+    "python [args[0]] [targets[0]] [targets[1]]",
+    depends=task5,
+    targets=[COUNT_FILE, PUBLIC_COUNT_FILE],
+    args=args.count_script)
+
+# transfer the updated counts files
+for filename in [COUNT_FILE, PUBLIC_COUNT_FILE]:
+    workflow.add_task(
+        "rsync -e 'ssh -i [args[0]] -l [args[1]]' '[depends[0]]' [args[2]]:[args[3]]",
+        depends=[filename,task6],
+        args=[args.key, args.user, args.remote, os.path.abspath(os.path.join(args.output_transfer,os.pardir))+"/"])
 
 workflow.go()
 
